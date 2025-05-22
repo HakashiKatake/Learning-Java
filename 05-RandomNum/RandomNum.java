335e873402d5f9eb8732cfea5981dcb743573262
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-22 23:18:11 +0530

    random class

---
+++ 05-RandomNum/RandomNum.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+
+public class RandomNum {
+    public static void main(String[] args) {
+      Random random = new Random();
+
+      int number1;
+      int number2;
+      int number3;
+
+      number1 = random.nextInt(16);
+      System.out.println(number1);
+
+
+    }
+}
