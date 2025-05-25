b1b19ae862cead44a103ae31b5b69d041ca33586
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-25 20:07:24 +0530

    objects

---
+++ 14-OOP/OOP.java
@@ -0,0 +1,29 @@
+
+public class OOP {
+    public static void main(String[] args) {
+        Car car = new Car();
+        // car.isRunning = true;
+        // System.out.println(car.model);
+
+        // System.out.println(car.make);
+        // System.out.println(car.year);
+        // System.out.println(car.price);
+        // System.out.println(car.isRunning);
+
+        // System.out.println(car.isRunning);
+        // car.start();
+        // System.out.println(car.isRunning);
+        // car.stop();
+        // System.out.println(car.isRunning);
+
+        // car.drive();
+        // car.brake();
+
+
+        
+
+        
+
+        
+    }
+}
