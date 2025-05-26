9d6f50093014cb4c1f0f64646953d823762afa15
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-26 15:23:16 +0530

    interface

---
+++ 17-Interface/Interface.java
@@ -0,0 +1,12 @@
+
+public class Interface {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+        rabbit.flee();
+        hawk.hunt();
+        fish.hunt();
+        fish.flee();
+    }
+}
---
+++ 17-Interface/Rabbit.java
@@ -0,0 +1,8 @@
+
+
+public class Rabbit implements Prey {
+    @Override
+    public void flee() {
+        System.out.println("The rabitt is running away");
+    }
+}
