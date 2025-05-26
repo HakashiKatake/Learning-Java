2379b664ed4f682126ea5b4bf27a9f86987c66f3
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-26 20:43:06 +0530

    wrapper classes

---
+++ 22-WrapperClasses/Wrapper.java
@@ -0,0 +1,55 @@
+
+public class Wrapper {
+    public static void main(String[] args) {
+        // Integer a = new Integer(123);
+        // Double b = new Double(3.14);
+        // Character c = new Character('$');
+        // Boolean d = new Boolean(true);
+
+
+
+        // or 
+
+
+        //autoboxing
+        // Integer a = 123;
+        // Double b = 3.14;
+        // Character c = '$';
+        // Boolean d = true;
+        
+
+        //unboxing
+        // int x = a;
+        //change data type for others 
+
+        // String a = Integer.toString(123);
+        // String b = Double.toString(3.14);
+        // String c= Character.toString('#');
+        // String d = Boolean.toString(false); 
+
+        //parsing
+
+        // int a = Integer.parseInt("124");
+        // double b = Double.parseDouble("3.14");
+        // char c = "Pizza".charAt(0);
+        // boolean d = Boolean.parseBoolean("true");
+
+        // String x = a + b + c + d;
+        // System.out.println(x);
+    
+
+        char letter = 'b';
+        System.out.println(Character.isLetter(letter));
+
+        System.out.println(Character.isUpperCase(letter));
+
+        
+
+
+
+
+
+
+
+    }
+}
